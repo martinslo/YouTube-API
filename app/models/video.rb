429871ed0cd1c9522f4c86898cfd6025ced6c052
Lucas de Oliveira <lucasmo@outlook.com.br>
2018-12-11 11:00:42 -0200

    Validação de campos
    Validação de campos, só aceita vídeos que contenham título, url e usuário presente

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,6 @@
 class Video < ApplicationRecord
   belongs_to :user
+    
+  # Validação de usuário, só aceita videos com título, url e usuário presente
+  validates :titulo, :url, :user, presence: true
 end
