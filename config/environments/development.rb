2b062771c89da11b99a00740d7375b06b5eaac96
Lucas de Oliveira <lucasmo@outlook.com.br>
2018-12-11 10:00:54 -0200

    Ajuste Devise
    Ajuste para tornar o localhost:3000 a URL padrão no desenvolvimento.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,9 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+  
+  # URL padrão para o Devise é o localhost:3000
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
   # Raises error for missing translations
