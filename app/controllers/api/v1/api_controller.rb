8b9301ad3df10b7c91d955d3cca7d8f80afe5cc6
Lucas de Oliveira <lucasmo@outlook.com.br>
2018-12-11 10:33:00 -0200

    Verificação de Usuário
    Verificação de Usuário para que o usuário que acesse a API seja um usuário válido no banco.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,12 @@
 module Api::V1
  class ApiController < ApplicationController
-   # Métodos globais
- end
+   # Verificar se o usuário que está acessando a API é um usuário válido no banco
+   acts_as_token_authentication_handler_for User
+   before_action :require_authentication!
+
+     private
+        def require_authentication!
+            throw(:warden, scope: :user) unless current_user.presence
+        end
+    end
 end
