793130af706c88aafbc55cbd2270239b1d2714b7
Lucas de Oliveira <lucasmo@outlook.com.br>
2018-12-11 10:28:08 -0200

    Adicionando token de autenticação
    Adicionando token de autenticação na tabela User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_120858) do
+ActiveRecord::Schema.define(version: 2018_12_11_122606) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_12_11_120858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
